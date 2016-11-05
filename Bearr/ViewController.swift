22259a6a4890923852fc8de5460138efccdcc09a
marcbaldwin <marc.baldwin88@gmail.com>
2016-11-05 11:28:25 +0000

    Initial Commit

---
+++ Bearr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bearr
+//
+//  Created by Marc Baldwin on 05/11/2016.
+//  Copyright © 2016 Marc Baldwin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
