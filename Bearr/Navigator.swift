26b59fd4d28c8b20075d98c70b254b9f606fc6fe
marcbaldwin <marc.baldwin88@gmail.com>
2016-11-09 12:33:29 +0000

    Extracts navigation logic into Navigator

--- Bearr/Navigator.swift
+++ Bearr/Navigator.swift
@@ -1,8 +1,55 @@
 import CoreLocation
+import RxSwift
 
-class Navigator {
+class Navigator: NSObject {
 
-    var bearing: Double = 0
+    fileprivate let locationManager = CLLocationManager()
+    fileprivate let destinationBearing = BehaviorSubject<CLLocationDirection>(value: 0)
+
+    let magneticHeading = BehaviorSubject<CLLocationDirection>(value: 0)
+    let destinationHeading: Observable<CLLocationDirection>
+
+    fileprivate var destination: CLLocation = CLLocation(latitude: 50.796346, longitude: -1.881024)
+
+    override init() {
+        destinationHeading = Observable.combineLatest(magneticHeading, destinationBearing) { $0 + $1 }
+    }
+
+    func start() {
+        locationManager.delegate = self
+        locationManager.startUpdatingHeading()
+
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+
+    func stop() {
+        locationManager.stopUpdatingHeading()
+        locationManager.stopUpdatingLocation()
+        locationManager.delegate = nil
+    }
+}
+
+extension Navigator: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        magneticHeading.onNext(-newHeading.magneticHeading)
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let currentLocation = locations.last {
+            destinationBearing.onNext(bearing(fromLocation: currentLocation, toLocation: destination))
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+        }
+    }
 }
 
 func bearing(fromLocation: CLLocation, toLocation: CLLocation) -> CLLocationDirection {
--- Bearr/ViewController.swift
+++ Bearr/ViewController.swift
@@ -1,86 +1,60 @@
 import UIKit
 import CoreLocation
+import RxSwift
 
 class ViewController: UIViewController {
-
-    fileprivate let locationManager = CLLocationManager()
 
     fileprivate lazy var pointerView = TriangleView(color: .blue)
     fileprivate lazy var northPointerView = TriangleView(color: .red)
 
-    fileprivate var doit = true
+    fileprivate var layoutSubviews = true
 
-    var destination: CLLocation = CLLocation(latitude: 50.720893, longitude: -1.879134)
-    var currentLocation: CLLocation? {
-        didSet { updatePointer() }
-    }
-
-    var northHeading: CLLocationDirection? {
-        didSet { updatePointer() }
-    }
+    fileprivate let navigator = Navigator()
+    fileprivate let disposeBag = DisposeBag()
 
     override func loadView() {
-        locationManager.delegate = self
-
         view = UIView()
         view.backgroundColor = .white
         view.addSubview(northPointerView)
         view.addSubview(pointerView)
+
+        navigator.magneticHeading.asObservable()
+            .subscribe(onNext: updateNorthPointer(bearing:))
+            .addDisposableTo(disposeBag)
+
+        navigator.destinationHeading
+            .subscribe(onNext: updatePointer(bearing:))
+            .addDisposableTo(disposeBag)
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        if doit {
+        if layoutSubviews {
             let width = min(view.bounds.width, view.bounds.height) * 0.25
             let height = (width / 2) * sqrt(3)
             let x = view.bounds.midX - (width / 2)
             let y = view.bounds.midY - (height / 2)
             pointerView.frame = CGRect(x: x, y: y, width: width, height: height)
             northPointerView.frame = CGRect(x: x, y: y, width: width, height: height)
-            doit = false
+            layoutSubviews = false
         }
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
-        locationManager.startUpdatingHeading()
-
-        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
-            locationManager.startUpdatingLocation()
-        } else {
-            locationManager.requestWhenInUseAuthorization()
-        }
+        navigator.start()
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        locationManager.stopUpdatingLocation()
+        navigator.stop()
     }
 
-    func updatePointer() {
-        if let currentLocation = self.currentLocation, let northBearing = self.northHeading {
-            let currentBearing = bearing(fromLocation: currentLocation, toLocation: destination)
-            let desiredBearing = currentBearing - northBearing
-            pointerView.transform = CGAffineTransform(rotationAngle: CGFloat(desiredBearing.toRadians()))
-        }
-    }
-}
-
-extension ViewController: CLLocationManagerDelegate {
-
-    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
-        northPointerView.transform = CGAffineTransform(rotationAngle: CGFloat(-newHeading.magneticHeading).toRadians())
-        northHeading = newHeading.magneticHeading
+    func updateNorthPointer(bearing: Double) {
+        northPointerView.transform = CGAffineTransform(rotationAngle: CGFloat(bearing.toRadians()))
     }
 
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        currentLocation = locations.last
-    }
-
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        if status == .authorizedWhenInUse {
-            locationManager.startUpdatingLocation()
-        }
+    func updatePointer(bearing: Double) {
+        pointerView.transform = CGAffineTransform(rotationAngle: CGFloat(bearing.toRadians()))
     }
 }
